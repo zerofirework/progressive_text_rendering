d30849a5a81bcb5801143173efa99ca4a2c1c378
Zero Liu <zerolxy@gmail.com>
2022-01-27 23:56:06 -0800

    Init commit

---
+++ script.js
@@ -0,0 +1,12 @@
+import { urls, getJson } from './api.js';
+
+const contentDiv = document.getElementById('content');
+
+function start() {
+  // Load the data using the provided API and display them in the content div in
+  // order.
+}
+
+document.getElementById('startBtn').addEventListener('click', () => {
+  start();
+});
